3b206255c32932b350c1dad2872bc223772b51e0
MaartenLat <63650005+MaartenLat@users.noreply.github.com>
2025-09-17 14:06:22 +0200

    Add files via upload

---
+++ Medical data visualizer/medical_data_visualizer.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = pd.read_csv('medical_examination.csv')
+# 2
+df['overweight'] = ( (df['weight']/((df['height']/100)**2)) > 25 ).astype(int)
+# 3
+df.replace({'cholesterol':{1:0,2:1,3:1},'gluc':{1:0,2:1,3:1}}, inplace=True)
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat=pd.melt(df, id_vars=['cardio'], value_vars=['alco', 'active', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    # 6
+    df_cat['total'] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).count()
+    # 7
+
+    # 8
+    fig = sns.catplot(y = 'total', x = 'variable', data = df_cat, kind = 'bar', col = 'cardio', hue ='value').fig
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+# 10
+def draw_heat_map():
+    # 11
+    mask_heat = (df['ap_lo']<=df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))
+    df_heat = df.loc[mask_heat]
+    # 12
+    corr = df_heat.corr()
+    # 13
+    mask = np.zeros(corr.shape,dtype='bool')
+    mask[np.triu_indices(len(corr))] = True
+    mask = pd.DataFrame(mask, index=corr.index, columns=corr.columns)
+    # 14
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # 15
+    sns.heatmap(corr, mask= mask, ax=ax, square=True, annot=True, fmt=".1f", linewidths=1)
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
